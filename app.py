6ac96fb114d6c7c563c6e548d8c6167c333dab4f
seenstevo <seenstevo@protonmail.com>
2023-03-21 12:24:01 +0100

    comment app

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def predict():
 
 
 
-if __name__ == "__main__":
-    app.run(host = 'localhost', port = 5000, debug = True)
+#if __name__ == "__main__":
+#    app.run(host = 'localhost', port = 5000, debug = True)
